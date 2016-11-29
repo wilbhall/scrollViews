59ca8683a290e4127f17727f52b25ed10b624541
wilbhall <joseph.leichter@gmail.com>
2016-11-29 17:29:29 -0500

    Finished with Scollview lecture

--- scrollViews/ViewController.swift
+++ scrollViews/ViewController.swift
@@ -10,16 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var images = [UIImageView]()
+    
+    @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        var contentWidth: CGFloat = 0.0
+        
+        print("Scrollview width: \(scrollView.frame.size.width)")
+        let scrollWidth = scrollView.frame.size.width
+        for x in 0...2{
+            let image = UIImage(named: "icon\(x).png")
+            
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            
+            newX = scrollWidth/2 + scrollWidth * CGFloat(x)
+            contentWidth+=newX
+            scrollView.addSubview(imageView)
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height/2) - 75, width: 150, height: 150)
+            
+        }
+        //scrollView.backgroundColor = UIColor.purple
+        scrollView.clipsToBounds = false
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+        
+        print("Count \(images.count)")
     }
-
 
 }
 
